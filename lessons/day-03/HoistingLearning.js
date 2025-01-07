daaf41b9a8ec6463b2232b6d1111ac46f4a26bbc
Trang Personal <trangtruong1610@gmail.com>
2025-01-07 21:35:49 +0700

    lesson day-03

---
+++ lessons/day-03/HoistingLearning.js
@@ -0,0 +1,16 @@
+console.log(myVariable);
+var myVariable = 1;
+
+/**
+ * Compile to
+ * var myVariable;
+ * console.log(myVariable); -> undefined
+ * myVariable = 1; 
+ */
+
+/**
+ * Hoisting (dâng lên): dâng lên cao nhất trong scope của nó
+ * {}: scope/code block/code body
+ * let/const: NO hoisting
+ * var: hoisting, giá trị mặc định sẽ là undefined
+ */
