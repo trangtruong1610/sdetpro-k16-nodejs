48243bda9474e4ee46ef003efca4a0774f657a7e
trang.truong@gigacover.com <trang.truong@gigacover.com>
2025-01-07 21:26:24 +0700

    update lab 2.2

--- labs/lab-02/lab-2.2.js
+++ labs/lab-02/lab-2.2.js
@@ -2,7 +2,7 @@ const readline = require("readline-sync");
 
 let inputNum = Number(readline.question("Please input the number: "));
 
-let evenNum = (inputNum % 2 == 0)
+let evenNum = (inputNum % 2 === 0)
 let msg = evenNum ? "It's an even Number" : "It's odd number"
 console.log(msg);
 
